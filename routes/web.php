175959a3b2de528fd4abf0c6e32d0e77b9d86be7
DannZolik <danjuha1@gmail.com>
2024-05-05 22:35:45 +0300

    added functions to routes

--- routes/web.php
+++ routes/web.php
@@ -1,73 +1,49 @@
 <?php
 
+use App\Http\Controllers\AdvertCategoryController;
+use App\Http\Controllers\AdvertController;
+use App\Http\Controllers\PaidAdvertController;
+use App\Http\Controllers\UserDataController;
+use App\Http\Controllers\UserDataTypeController;
+use App\Http\Controllers\UserTypesController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api/v1')->group(function(){
-    Route::get('/adverts', function () {
-    });
-    Route::get('/adverts/{advertID}', function () {
-    });
-    Route::post('/adverts/create', function () {
-    });
-    Route::post('/adverts/update', function () {
-    });
-    Route::post('/adverts/delete', function () {
-    });
+    Route::get('/adverts', [AdvertController::class, 'get']);
+    Route::get('/adverts/{advertID}', [AdvertController::class, 'profile']);
+    Route::post('/adverts/create', [AdvertController::class, 'create']);
+    Route::post('/adverts/update', [AdvertController::class, 'update']);
+    Route::post('/adverts/delete', [AdvertController::class, 'delete']);
 
-    Route::get('/advertCategories', function () {
-    });
-    Route::get('/advertCategories/{advertCategoryID}', function () {
-    });
-    Route::post('/advertCategories/create', function () {
-    });
-    Route::post('/advertCategories/update', function () {
-    });
-    Route::post('/advertCategories/delete', function () {
-    });
+    Route::get('/advertCategories', [AdvertCategoryController::class, 'get']);
+    Route::get('/advertCategories/{advertCategoryID}', [AdvertCategoryController::class, 'profile']);
+    Route::post('/advertCategories/create', [AdvertCategoryController::class, 'create']);
+    Route::post('/advertCategories/update', [AdvertCategoryController::class, 'update']);
+    Route::post('/advertCategories/delete', [AdvertCategoryController::class, 'delete']);
 
-    Route::get('/PaidAdvert', function () {
-    });
-    Route::get('/PaidAdvert/{PaidAdvertID}', function () {
-    });
-    Route::post('/PaidAdvert/create', function () {
-    });
-    Route::post('/PaidAdvert/update', function () {
-    });
-    Route::post('/PaidAdvert/delete', function () {
-    });
+    Route::get('/PaidAdvert', [PaidAdvertController::class, 'get']);
+    Route::get('/PaidAdvert/{PaidAdvertID}', [PaidAdvertController::class, 'profile']);
+    Route::post('/PaidAdvert/create', [PaidAdvertController::class, 'create']);
+    Route::post('/PaidAdvert/update', [PaidAdvertController::class, 'update']);
+    Route::post('/PaidAdvert/delete', [PaidAdvertController::class, 'delete']);
 
-    Route::get('/UserData', function () {
-    });
-    Route::get('/UserData/{UserDataID}', function () {
-    });
-    Route::post('/UserData/create', function () {
-    });
-    Route::post('/UserData/update', function () {
-    });
-    Route::post('/UserData/delete', function () {
-    });
+    Route::get('/UserData', [UserDataController::class, 'get']);
+    Route::get('/UserData/{UserDataID}', [UserDataController::class, 'profile']);
+    Route::post('/UserData/create', [UserDataController::class, 'create']);
+    Route::post('/UserData/update', [UserDataController::class, 'update']);
+    Route::post('/UserData/delete', [UserDataController::class, 'delete']);
 
-    Route::get('/UserDataType', function () {
-    });
-    Route::get('/UserDataType/{UserDataTypeID}', function () {
-    });
-    Route::post('/UserDataType/create', function () {
-    });
-    Route::post('/UserDataType/update', function () {
-    });
-    Route::post('/UserDataType/delete', function () {
-    });
+    Route::get('/UserDataType', [UserDataTypeController::class, 'get']);
+    Route::get('/UserDataType/{UserDataTypeID}', [UserDataTypeController::class, 'profile']);
+    Route::post('/UserDataType/create', [UserDataTypeController::class, 'create']);
+    Route::post('/UserDataType/update', [UserDataTypeController::class, 'update']);
+    Route::post('/UserDataType/delete', [UserDataTypeController::class, 'delete']);
 
-    Route::get('/UserTypes', function () {
-    });
-    Route::get('/UserTypes/{UserTypeID}', function () {
-    });
-    Route::post('/UserTypes/create', function () {
-    });
-    Route::post('/UserTypes/update', function () {
-    });
-    Route::post('/UserTypes/delete', function () {
-    });
+    Route::get('/UserTypes', [UserTypesController::class, 'get']);
+    Route::get('/UserTypes/{UserTypeID}', [UserTypesController::class, 'profile']);
+    Route::post('/UserTypes/create', [UserTypesController::class, 'create']);
+    Route::post('/UserTypes/update', [UserTypesController::class, 'update']);
+    Route::post('/UserTypes/delete', [UserTypesController::class, 'delete']);
 });
 
 Route::get('/', function () {});
